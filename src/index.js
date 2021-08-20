2a8cb5ef3b48aadf82bcd4bc6b7c13ec61656ac0
OlyaArta <artanika8@gmail.com>
2021-08-20 19:11:27 +0300

    added some changes

--- src/index.js
+++ src/index.js
@@ -3,3 +3,12 @@ import './sass/main.scss';
 import galleryCard from './templates/gallery-card.hbs';
 import galleryList from './templates/gallery-list.hbs';
 import API from './js/api';
+
+const refs = {
+  input: document.getElementById('search-form'),
+  galleryList: document.getElementById('gallery'),
+};
+
+refs.input.addEventListener('input', onSearch);
+
+function onSearch(e) {}
--- src/js/api.js
+++ src/js/api.js
@@ -1,3 +1,12 @@
-function fetchGallery(search) {
-  return fetch();
+async function fetchGallery(search) {
+  const response = await fetch(
+    `https://pixabay.com/api/?image_type=photo&orientation=horizontal&q=${search}&page=1&per_page=12&key=22999065-3fa49f66c111c7f1f9c9e2a79`,
+  );
+  const newGallery = await response.json();
+  return newGallery;
+
+  // return fetch(`https://pixabay.com/api/?image_type=photo&orientation=horizontal&q=что_искать&page=номер_страницы&per_page=12&key=22999065-3fa49f66c111c7f1f9c9e2a79
+  // `).then(response => response.json());
 }
+
+export default { fetchGallery };
