b10fadf0e6f347337078e9234cb42dc83dc55d1f
OlyaArta <artanika8@gmail.com>
2021-08-22 14:31:42 +0300

    make something

--- src/index.js
+++ src/index.js
@@ -2,13 +2,56 @@ import './sass/main.scss';
 
 import galleryCard from './templates/gallery-card.hbs';
 import galleryList from './templates/gallery-list.hbs';
-import API from './js/api';
+//import API from './js/api';
 
 const refs = {
   input: document.getElementById('search-form'),
   galleryList: document.getElementById('gallery'),
+  loadMore: document.querySelector('[data-action="load-more"]'),
 };
 
-refs.input.addEventListener('input', onSearch);
+refs.input.addEventListener('input', fetchGallery);
 
-function onSearch(e) {}
+function fetchGallery(search) {
+  this.page = 1;
+  this.search = '';
+  /*  const response = await fetch(
+      `https://pixabay.com/api/?image_type=photo&orientation=horizontal&q=${this.search}&page=${this.page}&per_page=12&key=22999065-3fa49f66c111c7f1f9c9e2a79`,
+    );
+    const newGallery = await response.json();
+    return newGallery;
+  } catch (error) {
+    console.error(error);
+  }*/
+
+  return fetch(
+    `https://pixabay.com/api/?image_type=photo&orientation=horizontal&q=${this.search}&page=${this.page}&per_page=12&key=22999065-3fa49f66c111c7f1f9c9e2a79`,
+  ).then(response => response.json());
+}
+
+//export default { fetchGallery };
+
+function onSearch(e) {
+  const search = e.target.value;
+  refs.galleryList.innerHTML = '';
+  if (search.trim() === '') {
+    return;
+  }
+
+  // const firstPage = 1;
+  // const resetPage = 1;
+  // const nextPage = firstPage + 1;
+
+  fetchGallery(search).then(images => {
+    const galleryListHTML = galleryList(images);
+    refs.galleryList.innerHTML = galleryListHTML;
+  });
+}
+
+/*function imagesMarkup(newGallery) {
+  galleryList.insertAdjacentHTML('beforeend', galleryCard(newGallery));
+}
+
+function clearGallery() {
+  galleryList.innerHTML = '';
+}*/
--- src/js/api.js
+++ src/js/api.js
@@ -1,9 +1,15 @@
-async function fetchGallery(search) {
-  const response = await fetch(
-    `https://pixabay.com/api/?image_type=photo&orientation=horizontal&q=${search}&page=1&per_page=12&key=22999065-3fa49f66c111c7f1f9c9e2a79`,
-  );
-  const newGallery = await response.json();
-  return newGallery;
+async function fetchGallery() {
+  try {
+    //  this.page = 1;
+    //  this.search = '';
+    const response = await fetch(
+      `https://pixabay.com/api/?image_type=photo&orientation=horizontal&q=${this.search}&page=${this.page}&per_page=12&key=22999065-3fa49f66c111c7f1f9c9e2a79`,
+    );
+    const newGallery = await response.json();
+    return newGallery;
+  } catch (error) {
+    console.error(error);
+  }
 
   // return fetch(`https://pixabay.com/api/?image_type=photo&orientation=horizontal&q=что_искать&page=номер_страницы&per_page=12&key=22999065-3fa49f66c111c7f1f9c9e2a79
   // `).then(response => response.json());
